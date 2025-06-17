9afd810bd2a8b4cea07e6794f44be9ff14878839
Ramil <ismakv@gmail.com>
2025-06-17 22:51:02 +0400

    feat: add taiga ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
+import { TuiRoot } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.html',
-  styleUrl: './app.less'
+  styleUrl: './app.less',
 })
 export class App {
   protected title = 'DocViewer';
