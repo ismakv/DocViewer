af3ad12356218c123172bf2d5a9e327af30f4c74
Ramil <ismakv@gmail.com>
2025-06-22 23:35:13 +0400

    feat: add annotation

---
+++ src/app/features/document-viewer/ui/annotation/annotation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-annotation',
+  imports: [CommonModule],
+  templateUrl: './annotation.component.html',
+  styleUrl: './annotation.component.less',
+})
+export class AnnotationComponent {}
--- src/app/features/document-viewer/ui/document-viewer/document-viewer.component.ts
+++ src/app/features/document-viewer/ui/document-viewer/document-viewer.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentService } from '@features/document-viewer/service/document.service';
 import { DocumentStateService } from '@features/document-viewer/service/document-state.service';
+import { AnnotationComponent } from '../annotation/annotation.component';
 
 @Component({
   selector: 'app-document-viewer',
-  imports: [CommonModule],
+  imports: [CommonModule, AnnotationComponent],
   templateUrl: './document-viewer.component.html',
   styleUrl: './document-viewer.component.less',
 })
